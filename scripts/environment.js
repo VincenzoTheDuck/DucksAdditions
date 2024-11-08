9acb2e8dde2834e1d732d39d4b38ece910721104
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-11-07 18:28:21 -0800

    Update environment.js

--- scripts/environment.js
+++ scripts/environment.js
@@ -1,1 +1,6 @@
 const gridstone = extend(Floor, "gridstone", {});
+const gridgrass = extend(Floor, "gridgrass", {});
+const griddirt = extend(Floor, "griddirt", {});
+const gridmud = extend(Floor, "gridmud", {});
+const gridsand = extend(Floor, "gridsand", {});
+const darkGridsand = extend(Floor, "dark-gridsand", {});
