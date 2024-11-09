d14325e321e42b0459bcee89b829f0dc04f9a952
Vincent <157900951+VincenzoTheDuck@users.noreply.github.com>
2024-11-09 08:16:48 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 require("environment")
 
 // turrets
+const harbinger = extend(ItemTurret, "harbinger", {});
+
 const uproar = extend(ItemTurret, "uproar", {});
 const radiance = extend(ContinuousTurret, "radiance", {});
